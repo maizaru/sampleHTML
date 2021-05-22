65d9bed982c810f836e04f15c8c77d7d00f30f45
wideHighter <natseater@gmail.com>
2021-05-22 22:10:04 +0900

    終わり .gitignore追加

--- js/common.js
+++ js/common.js
@@ -1,7 +1,34 @@
 $(function () {
     $('.slick01').slick({
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 3000,
+        arrows: true,
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '100px',
+        focusOnSelect: true,
         dots: true,
     });
+
+    //ハンバーガーメニュー
+    $('.drawer').drawer();
+
+    // ページ上部スクロール
+    var pagetop = $('#page_top');
+    // ボタン非表示
+    pagetop.hide();
+
+    // 100px スクロールしたらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body, html').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+    // ページ上部スクロール
 });
